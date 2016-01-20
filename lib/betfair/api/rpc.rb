729a7fdfbbe49e592ae70081143e1d285ceb47f7
andy shaw <shawry@shawry.com>
2016-01-21 07:28:04 +1100

    Use Australian endpoints.

--- lib/betfair/api/rpc.rb
+++ lib/betfair/api/rpc.rb
@@ -2,7 +2,7 @@ module Betfair
   module API
     module RPC
       def self.extended(obj)
-        obj.endpoint = "https://api.betfair.com/exchange/betting/json-rpc/v1"
+        obj.endpoint = "https://api-au.betfair.com/exchange/betting/json-rpc/v1"
         obj.persistent_headers.merge!({
           "Accept" => "application/json",
           "Content-Type" => "application/json"
